55c53bbd91f02a9e8f74866fceefaa6794fcd588
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-08-03 03:06:56 -0700

    Modularize benchmark test

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-
 def preprocess(): 
     (X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
 
@@ -16,6 +15,8 @@ def preprocess():
     y_test_encoded = keras.utils.to_categorical(y_test, num_classes = 10, dtype = 'float32')
     return X_train_scaled, y_train_encoded
 
+def concat_data(x, y, val): 
+    return x[:val], y[:val]
 
 def get_model():
     model = keras.Sequential([
@@ -30,27 +31,40 @@ def get_model():
     return model
 
 
-X_train_scaled, y_train_encoded = preprocess()
-X_train_scaled = X_train_scaled[:100]
-y_train_encoded = y_train_encoded[:100]
-start = time.perf_counter()
-elapsed = [start]
-with tf.device('/CPU:0'):
-    model_cpu = get_model()
-    model_resnet101 = tf.keras.applications.resnet.ResNet101(weights=None, input_shape=(32, 32, 3), classes=10)
-    model_resnet101.compile(optimizer='SGD',
+def benchmark_model(a_model): 
+
+    a_model.compile(optimizer='SGD',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-    model_cpu.fit(X_train_scaled, y_train_encoded, epochs = 1)
-    elapsed.append(time.perf_counter())
+    startTime = time.perf_counter()
+    hist = a_model.fit(X_train_scaled, y_train_encoded, epochs = 1)
+    elapsed_time = time.perf_counter() - startTime
+    #  accuracy = hist.history['accuracy'][-1]
+    best_accuracy = hist.history['accuracy'][argmin(history.history['loss'])]
+    return elapsed_time, best_accuracy
 
-    model_resnet101.fit(X_train_scaled, y_train_encoded, epochs = 1)
-    elapsed.append(time.perf_counter())
 
-print("elapsed")
-print(elapsed)
-for i in range(1, len(elapsed)): 
-    print("Elapsed %.3f seconds" % (elapsed[i] - elapsed[i - 1]))
+X_train_scaled, y_train_encoded = concat_data(*preprocess(), 100)
+#  start = time.perf_counter()
+#  elapsed = [start]
+
+model_list = {
+        "resnet101": tf.keras.applications.resnet.ResNet101(weights=None, input_shape=(32, 32, 3), classes=10)
+
+        }
+
+with tf.device('/CPU:0'):
+    for model_name in model_list: 
+
+        elapsed_time, best_accuracy = benchmark_model(model_list[model_name])
+        print("model_name: ", model_name)
+        print("elapsed_time: ", elapsed_time)
+        print("best_accuracy: ", best_accuracy)
+        print("---------------")
+
+#  print("elapsed: ", elapsed)
+#  for i in range(1, len(elapsed)): 
+#      print("Elapsed %.3f seconds" % (elapsed[i] - elapsed[i - 1]))
 
 
