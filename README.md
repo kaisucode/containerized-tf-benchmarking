b60cb7781cec3809f887f4d1643e6983d7a544fd
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-08-03 03:32:02 -0700

    Add readme and formatting fixes

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+### How to run
+`python main.py --num_epochs=1 --num_data=50 --is_gpu`
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import argparse
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -11,47 +12,63 @@ def preprocess():
     X_train_scaled = X_train/255
     X_test_scaled = X_test/255
 
-    y_train_encoded = keras.utils.to_categorical(y_train, num_classes = 10, dtype = 'float32')
-    y_test_encoded = keras.utils.to_categorical(y_test, num_classes = 10, dtype = 'float32')
+    y_train_encoded = keras.utils.to_categorical(y_train, num_classes=10, dtype='float32')
+    y_test_encoded = keras.utils.to_categorical(y_test, num_classes=10, dtype='float32')
     return X_train_scaled, y_train_encoded
 
 def concat_data(x, y, val): 
     return x[:val], y[:val]
 
-def benchmark_model(a_model): 
+def benchmark_model(a_model, args): 
 
     a_model.compile(optimizer='SGD',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
     startTime = time.perf_counter()
-    hist = a_model.fit(X_train_scaled, y_train_encoded, epochs = 3)
+    hist = a_model.fit(X_train_scaled, y_train_encoded, epochs=args.num_epochs)
     elapsed_time = time.perf_counter() - startTime
-    #  accuracy = hist.history['accuracy'][-1]
-    #  print(hist.history)
     best_accuracy = hist.history['accuracy'][np.argmin(hist.history['loss'])]
     return elapsed_time, best_accuracy
+    #  accuracy = hist.history['accuracy'][-1]
+    #  print(hist.history)
 
+def parseArguments(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_epochs", type=int, default=10)
+    parser.add_argument("--num_data", type=int, default=50)
+    parser.add_argument("--is_gpu", action="store_true")
+    args = parser.parse_args()
+    return args
 
-X_train_scaled, y_train_encoded = concat_data(*preprocess(), 10)
-#  start = time.perf_counter()
-#  elapsed = [start]
+def pretty_print_info(model_name, elapsed_time, best_accuracy): 
+    print("model_name: ", model_name)
+    print("elapsed_time: ", elapsed_time)
+    print('best_accuracy: {0:.2f}%'.format(best_accuracy))
+    print("---------------")
 
-model_list = {
-        "resnet101": tf.keras.applications.resnet.ResNet101(weights=None, input_shape=(32, 32, 3), classes=10)
+if __name__ == "__main__": 
 
-        }
+    args = parseArguments()
 
-with tf.device('/CPU:0'):
-    for model_name in model_list: 
-        elapsed_time, best_accuracy = benchmark_model(model_list[model_name])
-        print("---------------")
-        print("model_name: ", model_name)
-        print("elapsed_time: ", elapsed_time)
-        print("best_accuracy: ", best_accuracy)
+    X_train_scaled, y_train_encoded = concat_data(*preprocess(), args.num_data)
+    model_list = {
+            "resnet101": tf.keras.applications.resnet.ResNet101(weights=None, input_shape=(32, 32, 3), classes=10),
+            "resnet152": tf.keras.applications.resnet.ResNet152(weights=None, input_shape=(32, 32, 3), classes=10),
+            "vgg16": tf.keras.applications.VGG16(weights=None, input_shape=(32, 32, 3), classes=10)
+            }
 
-#  print("elapsed: ", elapsed)
-#  for i in range(1, len(elapsed)): 
-#      print("Elapsed %.3f seconds" % (elapsed[i] - elapsed[i - 1]))
+    if args.is_gpu: 
+        print("Benchmarking with GPU")
+        with tf.device('/GPU:0'):
+            for model_name in model_list: 
+                elapsed_time, best_accuracy = benchmark_model(model_list[model_name], args)
+                pretty_print_info(model_name, elapsed_time, best_accuracy)
+    else: 
+        print("Benchmarking with CPU")
+        with tf.device('/CPU:0'):
+            for model_name in model_list: 
+                elapsed_time, best_accuracy = benchmark_model(model_list[model_name], args)
+                pretty_print_info(model_name, elapsed_time, best_accuracy)
 
 
